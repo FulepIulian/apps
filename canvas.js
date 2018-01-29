8218061715a4a9640e39b32128341e2fa421d946
FulepIulian <32520257+FulepIulian@users.noreply.github.com>
2018-01-29 16:50:21 +0200

    Apps

---
+++ canvas.js
@@ -0,0 +1,17 @@
+
+document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.2";
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+
+context.beginPath();
+var w = canvas.getAttribute("width");
+var h = canvas.getAttribute("height");
+
+
+context.arc(w / 2, h / 2, 10, 0, 2 * Math.PI);
+
+
+
+context.stroke();
